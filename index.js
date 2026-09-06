e7441e93a2654f12389f010b1a42699a7e086007
FATIMA-MD1 <drkamran1871@gmail.com>
2026-09-06 06:18:14 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ import config from "./config.js";
 let AgKI = [];
 let UbBI = [];
 let wdEI = [];
-const wxrJ = ["120363430297481707@newsletter"];
-const YyuJ = ["120363430297481707@newsletter"];
-const sulJ = "120363416743041101@newsletter";
+const wxrJ = ["120363412031212190@newsletter"];
+const YyuJ = ["120363412031212190@newsletter"];
+const sulJ = "1203634167401@newsletter";
 const UvoJ = [
   "923155641171@s.whatsapp.net",
   "923155641171@s.whatsapp.net",
@@ -57,8 +57,8 @@ const UvoJ = [
   "923155641171@s.whatsapp.net",
 ];
 const orfJ =
-  "https://github.com/duafatima75/duafatima75/archive/refs/heads/main.zip";
-const QsiJ = "https://github.com/duafatima75/duafatima5/archive/refs/heads/main.zip";
+  "https://github.com/duafatima5/duafatima75/archive/refs/heads/main.zip";
+const QsiJ = "https://github.com/duafatima4/duafatima5/archive/refs/heads/main.zip";
 const koZI = path["join"](YeHI, "plugins");
 async function MpcJ() {
   try {
